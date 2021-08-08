f4ba96e6aea78266005eca6ffcd4140ea72af1d3
Konstantin Ivanin <ivanin.k@gmail.com>
2021-08-08 19:18:03 +0300

    Resolve task 701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte varByte;
+    short varShort;
+    int varInt;
+    long varLong;
+    float varFloat;
+    double varDouble;
+    boolean varBoolean;
+    char varChar;
+}
