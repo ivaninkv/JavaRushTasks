ad24fa8c947203c2a46330f560be64cc30732d50
Konstantin Ivanin <ivanin.k@gmail.com>
2021-08-09 16:28:17 +0300

    Some tasks 70* completed

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String varStr1 = new String("String1");
+        String varStr2 = new String("String2");
+        String varStr3 = new String("String3");
+        String varStr4 = new String("String4");
+        String varStr5 = new String("String5");
+        String varStr6 = new String("String6");
+        String varStr7 = new String("String7");
+        String varStr8 = new String("String8");
+        String varStr9 = new String("String9");
+        String varStr10 = new String("String10");
+
+        int[] varInt1 = new int[10];
+        int[] varInt2 = new int[10];
+        int[] varInt3 = new int[10];
+        int[] varInt4 = new int[10];
+        int[] varInt5 = new int[10];
+
+        Scanner varSc1 = new Scanner("");
+        Scanner varSc2 = new Scanner("");
+    }
+}
