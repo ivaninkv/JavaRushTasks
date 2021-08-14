a96e07a4a6f3f1bde3fc8466c113fa9ea2b881f8
Konstantin Ivanin <ivanin.k@gmail.com>
2021-08-14 22:11:35 +0300

    Task 908 completed, but not accepted.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0908/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.pro.task09.task0908;
+
+/*
+Двоично-шестнадцатеричный конвертер
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    private static final String binSymbols = "01";
+    private static final String hexSymbols = "0123456789abcdef";
+    private static final String[] hexArray = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111",
+            "1000", "1001", "1010", "1011", "1100", "1101", "1100", "1110", "1111"};
+
+    public static void main(String[] args) {
+        String binaryNumber = "100111010000";
+        System.out.println("Двоичное число " + binaryNumber + " равно шестнадцатеричному числу " + toHex(binaryNumber));
+        String hexNumber = "9d0";
+        System.out.println("Шестнадцатеричное число " + hexNumber + " равно двоичному числу " + toBinary(hexNumber));
+    }
+
+    public static String toHex(String binaryNumber) {
+        //напишите тут ваш код
+        if (binaryNumber == null ||
+                binaryNumber.equals("") ||
+                binaryNumber.replaceAll("[" + binSymbols + "]", "").length() != 0) {
+            return "";
+        }
+        String hexNumber = "";
+        if (binaryNumber.length() % 4 != 0) {
+            binaryNumber = "0".repeat(4 - binaryNumber.length() % 4) + binaryNumber;
+        }
+        for (int i = 0; i < binaryNumber.length(); i += 4) {
+            String str = binaryNumber.substring(i, i + 4);
+            hexNumber += hexSymbols.charAt(Arrays.asList(hexArray).indexOf(str));
+        }
+        return hexNumber;
+    }
+
+    public static String toBinary(String hexNumber) {
+        //напишите тут ваш код
+        if (hexNumber == null ||
+                hexNumber.equals("") ||
+                hexNumber.replaceAll("[" + hexSymbols + "]", "").length() != 0) {
+            return "";
+        }
+        String binaryNumber = "";
+        for (int i = 0; i < hexNumber.length(); i++) {
+            binaryNumber += hexArray[hexSymbols.indexOf(hexNumber.charAt(i))];
+        }
+        return binaryNumber;
+    }
+}
