3b193d597b1cc520e7f10c8e2205c8abbef9dbaa
Konstantin Ivanin <ivanin.k@gmail.com>
2021-08-22 16:18:50 +0300

    Task 1316 completed.

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task13.task1316;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (JavarushQuest jrQuest : JavarushQuest.values()) {
+            System.out.println(jrQuest.ordinal());
+        }
+    }
+}
