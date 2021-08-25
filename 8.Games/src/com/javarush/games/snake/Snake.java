09d1250cae4f02fcb062c56ad321f1f0916ce848
Konstantin Ivanin <ivanin.k@gmail.com>
2021-08-25 20:40:07 +0300

    Snake: task 7 completed.

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -10,6 +10,9 @@ public class Snake {
     private List<GameObject> snakeParts = new ArrayList<>();
     private static final String HEAD_SIGN = "\uD83D\uDC7E";
     private static final String BODY_SIGN = "\u26AB";
+    private Direction direction = Direction.LEFT;
+
+    public boolean isAlive = true;
 
 
     public Snake(int x, int y) {
@@ -19,8 +22,14 @@ public class Snake {
     public void draw(Game game) {
         for (int i = 0; i < snakeParts.size(); i++) {
             String symbol = i == 0 ? HEAD_SIGN : BODY_SIGN;
-            game.setCellValue(snakeParts.get(i).x, snakeParts.get(i).y, symbol);
+            GameObject gameObject = snakeParts.get(i);
+            Color snakeColor = isAlive ? Color.DARKORCHID : Color.RED;
+            game.setCellValueEx(gameObject.x, gameObject.y, Color.NONE, symbol, snakeColor, 75);
 
         }
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
     }
 }
