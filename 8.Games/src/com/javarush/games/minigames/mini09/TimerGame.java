d61d1782c57c0527a91f51bd25a7887fd694241c
Konstantin Ivanin <ivanin.k@gmail.com>
2021-09-19 09:49:19 +0300

    Minigame 9 completed.

---
+++ 8.Games/src/com/javarush/games/minigames/mini09/TimerGame.java
@@ -0,0 +1,30 @@
+package com.javarush.games.minigames.mini09;
+
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+/* 
+Таймер
+*/
+
+public class TimerGame extends Game {
+
+    @Override
+    public void initialize() {
+        setScreenSize(3, 3);
+        setTurnTimer(1000);
+    }
+
+    //напишите тут ваш код
+
+    @Override
+    public void onTurn(int step) {
+        setCellNumber(1, 1, step);
+        boolean even = step % 2 == 0;
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                setCellColor(x, y, even ? Color.GREEN : Color.ORANGE);
+            }
+        }
+    }
+}
