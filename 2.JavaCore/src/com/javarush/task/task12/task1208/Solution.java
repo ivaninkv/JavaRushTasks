35ef3656b874204604eed1887d6643a2b68bc842
Konstantin Ivanin <ivanin.k@gmail.com>
2021-09-20 21:01:43 +0300

    JavaCore. Task 1208 completed.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int a) {
+
+    }
+
+    public static void print(String a) {
+
+    }
+
+    public static void print(byte a) {
+
+    }
+
+    public static void print(int a, int b) {
+
+    }
+
+    public static void print(String s, int a) {
+
+    }
+}
