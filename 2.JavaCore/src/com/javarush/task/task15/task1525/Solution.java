288deb6c407ee73f580cdb0d39e3ba66277fdfc7
Konstantin Ivanin <ivanin.k@gmail.com>
2021-11-21 12:42:50 +0300

    JavaCore. Task 1525 completed.

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+Файл в статическом блоке
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+
+    static {
+        try (BufferedReader br = new BufferedReader(new FileReader(Statics.FILE_NAME))) {
+            lines = br.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
