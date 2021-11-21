245b748760567c4a48a09140d15f7a67fd2c9aab
Konstantin Ivanin <ivanin.k@gmail.com>
2021-11-21 12:20:47 +0300

    JavaCore. Task 1523 completed.

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task15.task1523;
+
+/*
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    private Solution(int a) {
+    }
+
+    Solution(String s) {
+    }
+
+    protected Solution(Number number) {
+    }
+
+    public Solution() {
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    SubSolution(String s) {
+        super(s);
+    }
+
+    protected SubSolution(Number number) {
+        super(number);
+    }
+
+    public SubSolution() {
+    }
+}
