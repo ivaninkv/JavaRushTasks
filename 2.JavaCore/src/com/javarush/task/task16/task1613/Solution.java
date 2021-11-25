87bb5f905a9a08cb8c7a0c62956bd3f9cf2595d8
Konstantin Ivanin <ivanin.k@gmail.com>
2021-11-25 22:03:20 +0300

    JavaCore. Task 1613 complete.

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1613/Solution.java
@@ -0,0 +1,75 @@
+package com.javarush.task.task16.task1613;
+
+/*
+Big Ben clock
+*/
+
+public class Solution {
+    public static volatile boolean isStopped = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Clock clock = new Clock("Лондон", 23, 59, 57);
+        Thread.sleep(4000);
+        isStopped = true;
+        Thread.sleep(1000);
+    }
+
+    public static class Clock extends Thread {
+        private String cityName;
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        public Clock(String cityName, int hours, int minutes, int seconds) {
+            this.cityName = cityName;
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            start();
+        }
+
+        public void run() {
+            try {
+                while (!isStopped) {
+                    printTime();
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        private void printTime() throws InterruptedException {
+            //add your code here - добавь код тут
+            Thread.sleep(1000);
+            boolean increaseParent = false;
+            if (seconds < 59) {
+                seconds++;
+            } else {
+                seconds = 0;
+                increaseParent = true;
+            }
+            if (increaseParent) {
+                if (minutes < 59) {
+                    minutes++;
+                    increaseParent = false;
+                } else {
+                    minutes = 0;
+                    increaseParent = true;
+                }
+            }
+            if (increaseParent) {
+                if (hours < 23) {
+                    hours++;
+                    increaseParent = false;
+                } else {
+                    hours = 0;
+                    increaseParent = true;
+                }
+            }
+            if (hours == 0 && minutes == 0 && seconds == 0) {
+                System.out.printf("В г. %s сейчас полночь!%n", cityName);
+            } else {
+                System.out.printf("В г. %s сейчас %d:%d:%d!%n", cityName, hours, minutes, seconds);
+            }
+        }
+    }
+}
