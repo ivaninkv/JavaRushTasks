9ee6615c2d1294fcaf844ed54ff67f16b656a3df
Konstantin Ivanin <ivanin.k@gmail.com>
2021-12-07 21:15:29 +0300

    JavaCore. Task 1623 complete.

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1623/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task16.task1623;
+
+/*
+Рекурсивное создание нитей
+*/
+
+public class Solution {
+    static int count = 15;
+    static volatile int createdThreadCount;
+
+    public static void main(String[] args) {
+        System.out.println(new GenerateThread());
+    }
+
+    public static class GenerateThread extends Thread {
+        public GenerateThread() {
+            super(String.valueOf(++createdThreadCount));
+            this.start();
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s created", getName());
+        }
+
+        @Override
+        public void run() {
+            if (createdThreadCount < count) {
+                GenerateThread generateThread = new GenerateThread();
+                System.out.println(generateThread);
+            }
+        }
+    }
+}
